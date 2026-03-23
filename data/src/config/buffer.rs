b019933940514863dfd25d932de9f456398560c7
Andrew Baldwin <andrew@boldfriend.org>
2026-03-23 02:10:50 -0700

    Add server_messages.away setting for filtering RPL_AWAY messages.

--- data/src/config/buffer.rs
+++ data/src/config/buffer.rs
@@ -348,6 +348,7 @@ pub struct ServerMessages {
     pub wallops: ServerMessage,
     pub kick: ServerMessage,
     pub change_topic: ServerMessage,
+    pub away: ServerMessage,
     pub default: ServerMessageDefault,
 }
 
@@ -375,6 +376,7 @@ impl ServerMessages {
             source::server::Kind::WAllOps => &self.wallops,
             source::server::Kind::Kick => &self.kick,
             source::server::Kind::ChangeTopic => &self.change_topic,
+            source::server::Kind::Away => &self.away,
         }
     }
 
@@ -513,7 +515,8 @@ impl Condensation {
             | source::server::Kind::MonitoredOffline
             | source::server::Kind::StandardReply(_)
             | source::server::Kind::WAllOps
-            | source::server::Kind::ChangeTopic => false,
+            | source::server::Kind::ChangeTopic
+            | source::server::Kind::Away => false,
         }
     }
 
